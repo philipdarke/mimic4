26f459afc40ed362f43fa8b50c76bcf674a29cbb
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2024-08-28 12:11:37 +0100

    Lint code

--- build_database.py
+++ build_database.py
@@ -2,19 +2,20 @@ from pathlib import Path
 from typing import Final
 
 import duckdb
+from duckdb import DuckDBPyConnection
 
 DATA_DIRECTORY: Final = Path("data/mimic-iv-3.0/")
 
-con = duckdb.connect(database="mimic4.db")
+con: DuckDBPyConnection = duckdb.connect(database="mimic4.db")
 
 # Create tables
 with open("create.sql", "r") as file:
-    sql = file.read()
+    sql: str = file.read()
     con.execute(sql)
 
-# Populate remaining tables
+# Populate tables
 for file in DATA_DIRECTORY.glob("*/*.csv.gz"):
-    file_split = str(file).split("/")
-    table = file_split[2] + "." + file_split[3].split(".")[0]
+    file_split: list[str] = str(file).split("/")
+    table: str = file_split[2] + "." + file_split[3].split(".")[0]
     print(f"{file} -> {table}...")
     con.sql(f"INSERT INTO {table} SELECT * FROM read_csv('{file}')")
